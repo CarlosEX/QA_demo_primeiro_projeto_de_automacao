74db23caa06477968a4a78216396ede6c280fc65
Carlos Antonio <carlos22martinssilva@gmail.com>
2021-10-07 11:36:00 -0300

    REFATORACAO: aplico extract funcion para extrair o codigo do Main para um funcao recebendo por parametro uma url

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 
 
@@ -11,18 +10,27 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
-            using (IWebDriver driver = new FirefoxDriver()){
+            OpenBrowserWithAutomation("https://www.google.com/ncr");
+        }
+        private static void OpenBrowserWithAutomation(string url){
+            try
+            {
+                using (IWebDriver driver = new FirefoxDriver()){
                 
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
 
-                driver.Navigate().GoToUrl("https://www.google.com/ncr");
-                
-                driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
-                wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
-                IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
-                Console.WriteLine(firstResult.GetAttribute("textContent"));
-                Console.ReadKey();
-                
+                    driver.Navigate().GoToUrl(url);
+                    driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
+                    wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
+                    IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
+                    
+                    Console.WriteLine(firstResult.GetAttribute("textContent"));
+                    Console.ReadKey();
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
             }
         }
     }
